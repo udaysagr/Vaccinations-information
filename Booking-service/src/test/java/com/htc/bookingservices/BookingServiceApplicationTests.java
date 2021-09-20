3f313eef669136ade8244e3a403bebd0baf3d9bd
uday sagar <udaysagar8597@yahoo.com>
2021-09-20 17:57:02 +0530

    #Unit Testing and dependencies added

--- Booking-service/src/test/java/com/htc/bookingservices/BookingServiceApplicationTests.java
+++ Booking-service/src/test/java/com/htc/bookingservices/BookingServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.htc.bookingservices;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookingServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
--- User_Service/src/test/java/com/htc/userservice/UserserviceApplicationTests.java
+++ User_Service/src/test/java/com/htc/userservice/UserserviceApplicationTests.java
@@ -1,7 +1,16 @@
 package com.htc.userservice;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.htc.userservice.entity.UserService;
+import com.htc.userservice.service.UserServiceInterface;
 
 @SpringBootTest
 class UserserviceApplicationTests {
@@ -9,5 +18,23 @@ class UserserviceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	private UserServiceInterface userServiceInterface; 
 
+	@BeforeEach
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	
+	@Test
+	public void user_Insertion_Test() {
+		UserService userService=new UserService("name1",1222311,"name@example.com",43,"Male");
+		assertEquals(userServiceInterface.save(userService),userServiceInterface.findById(1222311L).get());
+	}
+	
+	@AfterEach
+	public void afterTest() {
+		System.out.println("After Test");
+	}
 }
